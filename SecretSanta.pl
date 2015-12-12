b02b6b7b578964f5ec2808553a5215741b0cbe11
OrangeKaitos <lordkaitos@hotmail.com>
2015-12-12 16:33:26 +0100

    Added last bit of code
    Still not tested though

--- SecretSanta.pl
+++ SecretSanta.pl
@@ -3,15 +3,31 @@ start(InputFileName) :-
     read(PeopleList), read(BlackList), 
     seen,
     length(PeopleList, Length),
-    ( not(0 is Length mod 2) -> print('Please note that the number of people are uneven') ),
-    findSecretSanta(PeopleList, BlackList).
+    ( not(0 is Length mod 2) -> print('Please note that the number of people are uneven \n') ),
+    findSecretSanta(PeopleList, BlackList, [], ReturnList),
+    print('Santa  :  Receiver \n'),
+    printAll(ReturnList).
 
-findSecretSanta([Santa|PeopleList],BlackList):-
+findSecretSanta([],_,ReturnList, ReturnList).
+findSecretSanta([Santa|PeopleList],BlackList, EndList, ReturnList):-
 	%% TODO
+	randomListElement(PeopleList, Receiver),
+	\+(member([Santa,Receiver],BlackList); 
+		member([Receiver,Santa],BlackList)"),
+	findSecretSanta(PeopleList,BlackList,[[Santa,Receiver]|EndList], ReturnList)
 	.
 
-randomListElement([], _):- fail.
+
+randomListElement([], null).
 randomListElement(PeopleList, El) :-
     length(PeopleList, Length),
     random(0, Length, Index),
     nth0(Index, PeopleList, El).
+
+printAll([]).
+printAll([[Santa,Receiver]|PeopleList]):-
+	print(Santa),
+	print(' : '),
+	print(Receiver),
+	print('\n'),
+	printAll(PeopleList).
